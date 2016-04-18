ad7e3c8a3c203e795ac666b4f5fa1c78955c1c37
Kurt Rose <=>
2016-04-18 00:13:31 -0700

    this performance is too good to be true

--- test/profile_target.py
+++ test/profile_target.py
@@ -1,14 +1,36 @@
 import time
+import subprocess
+import sys
+import atexit
+
 
 import test
 import _test
 
-contexts = _test.init_contexts()
 
-N_SEND = 100000
+def run():
+	server = _start("test_server.py")
+	time.sleep(0.1)
+	client = _start("test_client.py")
+	client.wait()
 
-s = time.time()
-_test.session_test(contexts[0], N_SEND)
-f = time.time()
 
-print "{0:0.2f}us per send+recv".format(1e6 * (f - s) / N_SEND)
+_PROCS = []
+
+
+def _start(module):
+	proc = subprocess.Popen([sys.executable, module])
+	_PROCS.append(proc)
+	return proc
+
+
+def _cleanup():
+	for proc in _PROCS:
+		try:
+			proc.kill()
+		except OSError:
+			pass
+
+
+if __name__ == "__main__":
+	run()
